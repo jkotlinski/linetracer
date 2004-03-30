da34c4bf10d1e9e8bef706c2af687e2bc004d68c
Administrat�r <Administratr@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2004-03-30 20:45:19 +0000

    Initial revision

---
+++ LineTracer/LineTracerDoc.h
@@ -0,0 +1,59 @@
+// LineTracerDoc.h : interface of the CLineTracerDoc class
+//
+
+#pragma once
+
+#include <vector>
+#include "layer.h"
+
+using namespace std;
+
+class CLineTracerDoc : public CDocument
+{
+protected: // create from serialization only
+	CLineTracerDoc();
+	DECLARE_DYNCREATE(CLineTracerDoc)
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+
+// Implementation
+public:
+	virtual ~CLineTracerDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	void SetInputImageFileName(CString FileName);
+	CString GetInputImageFileName(void);
+private:
+	CString m_InputBitmapFileName;
+
+	vector<CLayer*> m_Layers;
+
+	Bitmap *m_InputBitmap;
+	bool LoadImage(Bitmap** bitmap, CString *fileName);
+
+public:
+	void ProcessLayers(void);
+	Bitmap* GetInputBitmap(void);
+	CLayer* GetLayer(int layer);
+	afx_msg void OnParametersBinarizer();
+};
+
+
---
+++ LineTracer/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LineTracer.rc
+//
+#define IDD_PARAMDIALOG                 9
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_LineTracerTYPE              129
+#define IDC_EDIT1                       1000
+#define IDC_EDIT                        1000
+#define ID_OPERATIONS_BINARIZE          32771
+#define ID_OPERATION_BINARIZE           32772
+#define ID_FILE_OPENIMAGE               32773
+#define ID_PARAMETERS_BINARIZE          32774
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
