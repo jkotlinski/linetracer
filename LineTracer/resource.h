07fe4e460d2c1375bccd2a5744c5b49c5cfa0d5c
Administrat�r <Administratr@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2004-04-06 09:12:47 +0000

    no message

--- LineTracer/resource.h
+++ LineTracer/resource.h
@@ -16,13 +16,14 @@
 #define ID_VIEW_BINARIZER               32777
 #define ID_VIEW_SKELETONIZER            32778
 #define ID_FILE_EXPORTEPS               32779
+#define ID_VIEW_ORIGINAL                32780
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_COMMAND_VALUE         32781
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
