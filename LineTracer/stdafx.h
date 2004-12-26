4c381ab4ce6edc28b3b30de45c4077202741e0a4
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2004-12-26 18:13:00 +0000

    added "memdc.h" and Logger

--- LineTracer/stdafx.h
+++ LineTracer/stdafx.h
@@ -2,7 +2,8 @@
 // or project specific include files that are used frequently,
 // but are changed infrequently
 
-#pragma once
+#ifndef __STDAFX_H
+#define __STDAFX_H
 
 #ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
@@ -11,18 +12,22 @@
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
 #ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
+//lint -e{1923} yes - do this as macro
 #define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
 #endif
 
 #ifndef _WIN32_WINNT		// Allow use of features specific to Windows NT 4 or later.
+//lint -e{1923} yes - do this as macro
 #define _WIN32_WINNT 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
 #endif						
 
 #ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
+//lint -e{1923} yes - do this as macro
 #define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
 #endif
 
 #ifndef _WIN32_IE			// Allow use of features specific to IE 4.0 or later.
+//lint -e{1923} yes - do this as macro
 #define _WIN32_IE 0x0400	// Change this to the appropriate value to target IE 5.0 or later.
 #endif
 
@@ -40,3 +45,9 @@
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 #include "gdiplush.h"
+#include "memdc.h"
+
+#include "Logger.h"
+#define LOG CLogger::Instance()->Log
+
+#endif
