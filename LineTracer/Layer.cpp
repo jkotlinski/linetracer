7d89dddea10eb6147843174f997dd52d8d71885e
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2004-12-26 19:23:48 +0000

    * added PaintImage() virtual method  * lintified

--- LineTracer/Layer.cpp
+++ LineTracer/Layer.cpp
@@ -1,20 +1,31 @@
 #include "StdAfx.h"
-#include ".\layer.h"
+#include "layer.h"
+
+#include "LayerManager.h"
 
 CLayer::CLayer(void)
 : m_ImageProcessor(NULL)
-, m_IsVisible(false)
 , m_SketchImage(NULL)
+, m_IsVisible(false)
 , m_IsValid(false)
+, m_cachedBitmap(NULL)
 {
-	TRACE("init layer\n");
+	LOG("init layer\n");
 }
 
-CLayer::~CLayer(void)
-{
-	if(m_SketchImage!=NULL) {
-		delete m_SketchImage;
-		m_SketchImage=NULL;
+CLayer::~CLayer(void) {
+	try {
+		if(m_SketchImage!=NULL) {
+			delete m_SketchImage;
+			m_SketchImage=NULL;
+		}
+	}
+	catch (...) {
+		try {
+			LOG ("Caught exception!");
+		}
+		catch (...) {
+		}
 	}
 }
 
@@ -30,10 +41,16 @@ CSketchImage * CLayer::GetSketchImage(void)
 
 void CLayer::SetVisible(bool state)
 {
+	if ( state != m_IsVisible )
+	{
+		//visibility state changed! tell layer manager
+		CLayerManager *l_lm = CLayerManager::Instance();
+		l_lm->ChangedLayerVisibleState();
+	}
 	m_IsVisible=state;
 }
 
-bool CLayer::IsVisible(void)
+bool CLayer::IsVisible(void) const
 {
 	return m_IsVisible;
 }
@@ -41,18 +58,21 @@ bool CLayer::IsVisible(void)
 void CLayer::Process(CSketchImage *src)
 {
 	if(IsValid()) return;
+	ASSERT ( m_ImageProcessor != NULL );
 
-	if(m_SketchImage!=NULL) {
+	if (m_SketchImage != NULL) 
+	{
+		//LOG ( "m_SketchImage: %x\n", m_SketchImage );
 		delete m_SketchImage;
-		m_SketchImage=NULL;
+		m_SketchImage = NULL;
 	}
 
-	m_SketchImage=m_ImageProcessor->Process(src);
+	m_SketchImage = m_ImageProcessor->Process(src);
 
 	SetValid(true);
 }	
 
-bool CLayer::IsValid(void)
+bool CLayer::IsValid(void) const
 {
 	return m_IsValid;
 }
@@ -62,7 +82,26 @@ void CLayer::SetValid(bool isValid)
 	m_IsValid=isValid;
 }
 
-double CLayer::GetParam(char* name)
+double CLayer::GetParam(CImageProcessor::ParamName name)
 {
+	ASSERT ( m_ImageProcessor != NULL );
+	ASSERT ( name != NULL );
 	return m_ImageProcessor->GetParam(name);
+}
+
+CString* CLayer::GetName(void)
+{
+	ASSERT ( m_ImageProcessor != NULL );
+
+	CString *l_name = m_ImageProcessor->GetName();
+
+	return l_name;
+}
+
+void CLayer::PaintImage(CRawImage<ARGB>* a_canvas) const
+{
+	ASSERT ( a_canvas != NULL );
+	ASSERT ( m_ImageProcessor != NULL );
+
+	m_ImageProcessor->PaintImage ( m_SketchImage, a_canvas );
 }
