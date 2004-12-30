eed108241f9f5c2ef5f961460296880b5db5c184
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2004-12-30 12:27:31 +0000

    tidied up and optimized

--- trunk/LineTracer/LineImagePainter.h
+++ trunk/LineTracer/LineImagePainter.h
@@ -8,7 +8,7 @@ class CLineImagePainter
 public:
 	static void Paint(CRawImage<ARGB>* canvas, const CLineImage* li);
 private:
-	static void DrawLine(CRawImage<ARGB>* canvas, const CFPoint* start, const CFPoint* end, ARGB c);
+	static void DrawLine(CRawImage<ARGB>* canvas, const CPoint &start, const CPoint &end, ARGB c);
 	static void DrawPoint(CRawImage<ARGB>* canvas, const CFPoint* p, ARGB color);
 	static void DrawCurve(CRawImage<ARGB>* canvas, const CSketchPoint* start, const CSketchPoint* end, ARGB color);
 };
