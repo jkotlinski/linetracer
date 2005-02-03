d85fb1664b3ceb999c58618d7772752a27e97763
nobody <nobody@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2005-02-03 19:34:42 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_1_0'.

---
+++ tags/v0_1_0/LineTracer/BezierMaker.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "imageprocessor.h"
+
+#include "LineImage.h"
+#include "FPoint.h"
+
+class CBezierMaker :
+	public CImageProcessor
+{
+protected:
+	CBezierMaker(void);
+public:
+	static CBezierMaker* Instance();
+	~CBezierMaker(void);
+	CSketchImage* Process(CSketchImage* src);
+private:
+	CSketchImage* SimpleMethod(const CLineImage* src) const;
+	double FindError(CPolyLine* polyline, CPolyLine* curve, vector<double> *tlist, unsigned int &worstPoint) const;
+	CLineImage* DoSchneider(const CLineImage* src) const;
+	vector<double>* CalcT(CPolyLine* line) const;
+	vector<CFPoint>* CalcTangents(CPolyLine* line, const CSketchPoint* startTangent = NULL, const CSketchPoint* endTangent = NULL) const;
+	CPolyLine* FitSpline(CPolyLine* pl, vector<double>* tlist, vector<CFPoint>* tangentList) const;
+	CPolyLine* FitLine(CPolyLine* pl, const vector<CFPoint>* tangentList) const;
+	void ReparametrizeT(CPolyLine* pl, CPolyLine* curve, vector<double>* tlist, bool &a_foundBetterTListStatus) const;
+	bool ImprovementSmallEnough(double error, double previousError) const;
+};
---
+++ tags/v0_1_0/LineTracer/Binarizer.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "imageprocessor.h"
+
+class CBinarizer :
+	public CImageProcessor
+{
+protected:
+	CBinarizer(void);
+public:
+	static CBinarizer* Instance();
+	~CBinarizer(void);
+	CSketchImage* Process(CSketchImage* src);
+
+protected:
+	int CalculateOtsuThreshold(const CRawImage<unsigned char> *img) const;
+private:
+	int* m_sketchBoard;
+public:
+	void Init(void);
+public:
+	const CRawImage<int>* GetDistanceMap() const;
+private:
+	CRawImage<int>* m_distanceMap;
+	void CalcDistanceMap(const CRawImage<bool>* img);
+};
