78f76fdb22906ddff1b53b04498a4cf0984fd7f6
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2005-02-04 14:47:04 +0000

    added methods
    > 	void AssertNotEqualTo(const CPolyLine & a_otherLine);
    > 	const bool Equals(const CPolyLine& a_line);

--- trunk/LineTracer/PolyLine.h
+++ trunk/LineTracer/PolyLine.h
@@ -16,18 +16,18 @@ private:
 	vector<CSketchPoint*> m_points;
 public:
 	bool Contains(CPoint p);
-	unsigned int Size(void);
+	unsigned int Size(void) const;
 	CPolyLine* Clone(void);
 	int HasKnots(void);
-	CSketchPoint* GetHeadPoint(void);
-	CSketchPoint* GetTailPoint(void);
+	CSketchPoint* GetHeadPoint(void) const;
+	CSketchPoint* GetTailPoint(void) const;
 	CPolyLine* MergeLine(CPolyLine* line);
 	vector<CSketchPoint*>::iterator Begin(void);
 	vector<CSketchPoint*>::iterator End(void);
 	double GetMaxDeviation();
 	void Clear(void);
 	CSketchPoint* GetMaxDeviationPoint(void);
-	CSketchPoint* At(unsigned int i);
+	CSketchPoint* At(unsigned int i) const;
 	int GetMedianThickness(void);
 private:
 	bool m_isTail;
@@ -35,7 +35,7 @@ public:
 	void SetTail(bool val);
 	bool IsTail(void);
 	CPolyLine* SmoothPositions(void);
-	void Trace(void);
+	void Trace(void) const;
 	int RemoveDuplicatePoints(void);
 	void SmoothPoint(int a_pointIndex);
 	
@@ -46,4 +46,7 @@ public:
 private:
 	void DrawCurve(Graphics& a_graphics, Pen &a_pen, CSketchPoint* a_startPoint, CSketchPoint* a_endPoint);
 
+public:
+	void AssertNotEqualTo(const CPolyLine & a_otherLine);
+	const bool Equals(const CPolyLine& a_line);
 };
