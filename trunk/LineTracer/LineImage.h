8bf10c8cff8e78dc00fa3ae2323e0a7650323952
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2005-02-04 14:45:14 +0000

    added methods
    > 	void AssertNoEmptyLines(void);
    > 	void AssertNoDuplicateLines(void);
    > 	void DiscardDuplicateLines(void);

--- trunk/LineTracer/LineImage.h
+++ trunk/LineTracer/LineImage.h
@@ -24,7 +24,7 @@ public:
 	void Clear(void);
 	void SolderKnots(void);
 	CLineImage* Clone(void) const;
-	bool IsTail(map <unsigned int,int>* a_tailCounterMap, CPolyLine* pl) const;
+	bool IsTail(map <unsigned int,int>* a_tailCounterMap, const CPolyLine* pl) const;
 	CLineImage* SmoothPositions() const;
 	// check all lines in image and update their tail status
 	void UpdateTailData(void);
@@ -36,4 +36,8 @@ public:
 private:
 	void DrawCurve(Graphics& a_graphics, CSketchPoint* a_startPoint, CSketchPoint* a_endPoint);
 	void DrawLine(Graphics & a_graphics, CFPoint& a_startPoint, CFPoint& a_endPoint);
+public:
+	void AssertNoEmptyLines(void) const;
+	void AssertNoDuplicateLines(void) const;
+	void DiscardDuplicateLines(void);
 };
