ba24c2e000ab24d273fcb750b37163e460276e7d
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2005-02-13 22:52:08 +0000

    * fixed simple but nasty multithreading bug (caused by not sending enough Invalidate messages to main thread) * cleaned up code

--- trunk/LineTracer/LayerManager.cpp
+++ trunk/LineTracer/LayerManager.cpp
@@ -1,6 +1,5 @@
 #include "StdAfx.h"
 #include "layermanager.h"
-#include <process.h>
 
 #include "Logger.h"
 #include "RawImage.h"
@@ -26,6 +25,7 @@ CLayerManager::CLayerManager(void)
 , m_restartProcess(false)
 , m_lineTracerView(NULL)
 , m_isProcessing(false)
+, m_Layers()
 {
 	LOG("init layermanager\n");
 
@@ -105,8 +105,6 @@ CLayerManager* CLayerManager::Instance() {
 
 void CLayerManager::InvalidateLayers(unsigned int startLayer)
 {
-	LOG( "InvalidateLayers %i start\n", startLayer );
-
 	for(unsigned int i=startLayer; i<m_Layers.size(); i++) {
 		m_Layers.at(i)->SetValid(false);
 
@@ -114,8 +112,6 @@ void CLayerManager::InvalidateLayers(unsigned int startLayer)
 			CBinarizer::Instance()->Init();
 		}
 	}
-
-	LOG( "InvalidateLayers bye\n" );
 }
 
 void CLayerManager::ProcessLayers()
@@ -123,13 +119,13 @@ void CLayerManager::ProcessLayers()
 	if ( m_processThread != NULL )
 	{
 		m_restartProcess = true;
-		LOG ( "restartProcess = true\n" );
 	}
 	else 
 	{
-		LOG( "ProcessLayers() this=%x\n", this );
-		m_processThread = AfxBeginThread ( DoProcessLayers, (LPVOID)this );
-		LOG ( "m_processThread == %x\n", m_processThread );
+		m_processThread = AfxBeginThread ( 
+			DoProcessLayers, 
+			static_cast<LPVOID>( this )
+			);
 	}
 }
 
@@ -139,6 +135,7 @@ CLayer* CLayerManager::GetLayer(int layer)
 }
 
 unsigned int CLayerManager::LayerCount(void)
+const
 {
 	return static_cast<unsigned int> ( m_Layers.size() );
 }
@@ -177,7 +174,6 @@ UINT CLayerManager::DoProcessLayers(LPVOID pParam)
 	K_ACTIVE_PROCESSES++;
 
 	CLogger::Instance()->Activate();
-	LOG( "DoProcessLayers() %i enter\n", K_ACTIVE_PROCESSES );
 
 	CLayerManager *l_lm = static_cast<CLayerManager*>(pParam);
 
@@ -185,7 +181,11 @@ UINT CLayerManager::DoProcessLayers(LPVOID pParam)
 
 	ASSERT ( l_lm->LayerCount() > 0 );
 
+	int l_loopCount = 0;
+
 	do {
+		ASSERT ( l_loopCount++ < 1000 );
+
 		l_lm->m_restartProcess = false;
 
 		CLayer *layer = l_lm->GetLayer(0);
@@ -203,9 +203,9 @@ UINT CLayerManager::DoProcessLayers(LPVOID pParam)
 		
 			CLogger::Instance()->Activate();
 			LOG( "process done: %i\n", l_activeLayerIndex );
-			LOG ( "m_restartProcess: %x\n", l_lm->m_restartProcess );
 
-			if ( l_lm->m_restartProcess ) {
+			if ( l_lm->m_restartProcess ) 
+			{
 				break;
 			}
 
@@ -213,7 +213,10 @@ UINT CLayerManager::DoProcessLayers(LPVOID pParam)
 			ASSERT ( l_result != 0 );
 
 			//redraw!
-			l_lm->GetLineTracerView()->Invalidate( FALSE );
+			if ( layer->IsVisible() )
+			{
+				l_lm->GetLineTracerView()->Invalidate( FALSE );
+			}
 
 			img = layer->GetSketchImage();
 		}
@@ -229,6 +232,10 @@ UINT CLayerManager::DoProcessLayers(LPVOID pParam)
 	l_lm->m_isProcessing = false; 
 	
 	K_ACTIVE_PROCESSES--;
+
+	//redraw!
+	l_lm->GetLineTracerView()->Invalidate( FALSE );
+
 	return 0;
 }
 
@@ -260,45 +267,6 @@ void ErrorExit(LPTSTR lpszFunction)
     ExitProcess(dw); 
 }
 
-/*void CLayerManager::AbortOldThread(void)
-{
-	ASSERT ( 0 );
-	LOG ( "AbortOldThread() start\n" );
-
-	if ( m_processThread == NULL )
-	{
-		LOG ( "thread was null - no problem\n") ;
-		LOG ( "AbortOldThread() exit\n") ;
-		return;
-	}
-
-	// tell thread to terminate!
-	LOG ( "AbortProcessing()\n" );
-	//AbortProcessing();
-	HANDLE l_threadHandle = m_processThread->m_hThread;
-	LOG ( "WaitForSingleObject() start... %x\n", m_processThread );
-
-	DWORD l_waitResult = ::WaitForSingleObject ( l_threadHandle, 5000L );
-	
-	LOG ( "waitResult: %x\n", l_waitResult);
-	if ( l_waitResult != WAIT_OBJECT_0 )
-	{
-		// the thread is still running
-		ErrorExit(NULL);
-	}
-
-	m_processThread = NULL;
-
-	LOG ( "WaitForSingleObject() ...ok! %x\n", m_processThread );
-
-	//ClearAbortProcessingFlag();
-	LOG ( "AbortOldThread() bye\n" );
-}*/
-
-void CLayerManager::ChangedLayerVisibleState(void)
-{
-}
-
 void CLayerManager::SetLineTracerView(CView* a_lineTracerView)
 {
 	ASSERT ( a_lineTracerView != NULL );
@@ -313,15 +281,13 @@ CView* CLayerManager::GetLineTracerView(void)
 
 void CLayerManager::DrawAllLayers(Graphics & a_graphics)
 {
-	LOG ( "layermanager->DrawAllLayers()" );
-
-	bool l_noLayersVisible = true;
-
+	LOG ( "layermanager->DrawAllLayers()\n" );
+	
 	if ( IsProcessing() == false )
 	{
 		//clear background
 		SolidBrush l_whiteBrush (Color(255, 255, 255, 255));
-		a_graphics.FillRectangle(&l_whiteBrush, 0, 0, 
+		(void) a_graphics.FillRectangle(&l_whiteBrush, 0, 0, 
 			GetLayer(0)->GetImageWidth(),
 			GetLayer(0)->GetImageHeight() 
 			);
@@ -340,12 +306,7 @@ void CLayerManager::DrawAllLayers(Graphics & a_graphics)
 			{
 				continue;
 			}
-			CLogger::Activate();
-			LOG ( "painted: " );
-			LOG ( (LPCSTR)l_layer->GetName() );	
-			LOG ( "\n" );
 			l_layer->DrawUsingGraphics ( a_graphics );
-			l_noLayersVisible = false;
 		}
 	}
 }
@@ -360,7 +321,8 @@ void CLayerManager::SetVectorLayerVisibility(bool a_isVisible)
 	GetLastLayer()->SetVisible(a_isVisible);
 }
 
-bool CLayerManager::IsProcessing(void)
+bool CLayerManager::IsProcessing(void) 
+const
 {
 	return m_isProcessing;
 }
