eb9ae957a06a2ed0c72ea850f3857d86be2cdd35
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2005-02-22 21:07:23 +0000

    * main window position autosaves * toolbox docks to main window at start

--- LineTracer/MainFrm.cpp
+++ LineTracer/MainFrm.cpp
@@ -45,8 +45,8 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	if (CFrameWnd::OnCreate(lpCreateStruct) == -1)
 		return -1;
 
-	// Tell the frame window to permit docking.
-    EnableDocking (CBRS_ALIGN_ANY);
+	// Tell the frame window to forbid docking.
+    EnableDocking ( 0 );
 
 	if (!m_wndStatusBar.Create(this) ||
 		!m_wndStatusBar.SetIndicators(indicators,
@@ -56,10 +56,8 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 		return -1;      // fail to create
 	}
 
-	CToolBox *m_wndToolbox = CToolBox::Instance();
-
 	// Create the dialog bar using one of its Create() methods
-	if( !m_wndToolbox->Create(this,
+	if( !CToolBox::Instance()->Create(this,
 			          IDD_TOOLBOX,
 			          CBRS_LEFT | CBRS_GRIPPER | CBRS_TOOLTIPS | CBRS_FLYBY,
 			          IDD_TOOLBOX) )
@@ -69,15 +67,14 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	}
 
 	// When the dialog bar is undocked, display a caption on its title bar
-	m_wndToolbox->SetWindowText("Toolbox");
+	CToolBox::Instance()->SetWindowText("Toolbox");
 
-
-	// Allow the dialog bar to be dockable on the left or the right of the frame
-	m_wndToolbox->EnableDocking(0);
+	// disallow docking -- was: Allow the dialog bar to be dockable on the left or the right of the frame
+	CToolBox::Instance()->EnableDocking(0);
 	//DockControlBar(&m_wndToolbox);
-	FloatControlBar(m_wndToolbox, CPoint(0,0), CBRS_ALIGN_TOP);
-
+	
 	//LoadBarState(_T("ToolbarState"));
+	FloatControlBar(CToolBox::Instance(), CPoint(0,0), CBRS_ALIGN_TOP);
 
     return 0;
 }
@@ -111,5 +108,111 @@ void CMainFrame::Dump(CDumpContext& dc) const
 afx_msg void CMainFrame::OnClose(void)
 {
 	SaveBarState(_T("ToolbarState"));
-	CFrameWnd::OnClose();
+
+    WINDOWPLACEMENT    wp;
+
+    // before it is destroyed, save the position of the window
+    wp.length = sizeof wp;
+
+    if ( GetWindowPlacement(&wp) )
+    {
+        if ( IsIconic() )
+          // never restore to Iconic state
+          wp.showCmd = SW_SHOW ;
+
+        if ((wp.flags & WPF_RESTORETOMAXIMIZED) != 0)
+          // if maximized and maybe iconic restore maximized state
+          wp.showCmd = SW_SHOWMAXIMIZED ;
+
+        // and write it to the .INI file
+        WriteWindowPlacement(&wp);
+    }
+    
+    CFrameWnd::OnClose();
+}
+
+void CMainFrame::ActivateFrame(int nCmdShow)
+{
+    // nCmdShow is the normal show mode this frame should be in
+    // translate default nCmdShow (-1)
+    if (nCmdShow == -1)
+    {
+        if (!IsWindowVisible())
+            nCmdShow = SW_SHOWNORMAL;
+        else if (IsIconic())
+            nCmdShow = SW_RESTORE;
+    }
+
+    // bring to top before showing
+    BringToTop(nCmdShow);
+
+    if (nCmdShow != -1)
+    {
+        // show the window as specified
+      WINDOWPLACEMENT wp;
+      
+      if ( !ReadWindowPlacement(&wp) )
+      {
+          ShowWindow(nCmdShow);
+      }
+      else
+      {
+         if ( nCmdShow != SW_SHOWNORMAL )  
+           wp.showCmd = nCmdShow;
+
+         SetWindowPlacement(&wp);
+         // ShowWindow(wp.showCmd);
+      }
+
+      // and finally, bring to top after showing
+      BringToTop(nCmdShow);
+    }
+
+	CRect l_screenRect;
+	GetWindowRect(&l_screenRect);
+	FloatControlBar(CToolBox::Instance(), CPoint(l_screenRect.right,l_screenRect.top), CBRS_ALIGN_TOP);
+
+	return ;
+}
+
+static char szSection[]   = "Settings";
+static char szWindowPos[] = "WindowPos";
+static char szFormat[] = "%u,%u,%d,%d,%d,%d,%d,%d,%d,%d";
+
+BOOL CMainFrame::ReadWindowPlacement(WINDOWPLACEMENT *pwp)
+{
+    CString strBuffer;
+    int    nRead ;
+
+    strBuffer = AfxGetApp()->GetProfileString(szSection, szWindowPos);
+    if ( strBuffer.IsEmpty() )  return FALSE;
+
+    nRead = sscanf(strBuffer, szFormat,
+                &pwp->flags, &pwp->showCmd,
+                &pwp->ptMinPosition.x, &pwp->ptMinPosition.y,
+                &pwp->ptMaxPosition.x,&pwp->ptMaxPosition.y,
+                &pwp->rcNormalPosition.left,  &pwp->rcNormalPosition.top,
+                &pwp->rcNormalPosition.right, &pwp->rcNormalPosition.bottom);
+    if ( nRead != 10 )  return FALSE;
+    pwp->length = sizeof(WINDOWPLACEMENT);
+
+    return TRUE;
+}
+
+// Write a window placement to settings section of app's ini file.
+
+void CMainFrame::WriteWindowPlacement(WINDOWPLACEMENT *pwp)
+{
+    char szBuffer[sizeof("-32767")*8 + sizeof("65535")*2];
+    int max = 1;
+    CString s;
+
+    sprintf(szBuffer, szFormat,
+            pwp->flags, pwp->showCmd,
+            pwp->ptMinPosition.x, pwp->ptMinPosition.y,
+            pwp->ptMaxPosition.x, pwp->ptMaxPosition.y,
+            pwp->rcNormalPosition.left, pwp->rcNormalPosition.top,
+            pwp->rcNormalPosition.right, pwp->rcNormalPosition.bottom);
+     AfxGetApp()->WriteProfileString(szSection, szWindowPos, szBuffer);
+
 }
