f75a32d2b081ec52706fe5d9c0bf98544269e331
nobody <nobody@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2005-02-23 17:51:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_1_8'.

---
+++ tags/v0_1_8/LineTracer/ProjectSettings.cpp
@@ -0,0 +1,54 @@
+#include "StdAfx.h"
+#include "projectsettings.h"
+#include ".\projectsettings.h"
+
+CProjectSettings::CProjectSettings(void)
+: m_params()
+{
+	Init();
+}
+
+CProjectSettings::~CProjectSettings(void)
+{
+}
+
+CProjectSettings* CProjectSettings::Instance(void) {
+	static CProjectSettings l_instance;
+	return &l_instance;
+}
+
+double CProjectSettings::GetParam(CProjectSettings::ParamName name) {
+	LOG ( "settings->GetParam; name = %i\n", name );
+	LOG ( "m_params.count(name): %i\n", m_params.count(name) );
+	ASSERT ( m_params.count(name) == 1 );
+
+	if ( m_params.count ( name ) == 0 ) {
+		return -1.0;
+	}
+	double l_retval = m_params[name];
+	LOG ( "settings->GetParam; retval = %f\n", l_retval );
+	return l_retval;
+}
+
+void CProjectSettings::SetParam ( CProjectSettings::ParamName a_paramName, double a_value ) {
+	LOG ( "settings->SetParam %i = %f\n", a_paramName, a_value );
+	m_params[a_paramName] = a_value;
+}
+
+void CProjectSettings::Init(void)
+{
+	SetParam(BEZIERMAKER_ERROR_THRESHOLD, 20.0);
+	SetParam(BINARIZER_THRESHOLD, -1.0);
+	SetParam(BINARIZER_MEAN_C, 10.0);
+	SetParam(LINESEGMENTOR_THRESHOLD, 1.5);
+	SetParam(TAILPRUNER_THRESHOLD, 4.0);
+	SetParam(SKELETONIZER_SCALE, 1.0);
+	SetParam(KNEESPLITTER_THRESHOLD, -0.3);
+	SetParam(HOLEFILLER_MAX_AREA_TO_FILL, 10.0);
+	SetParam(GAUSSIAN_RADIUS, 0.5);
+}
+
+void CProjectSettings::Reset(void)
+{
+	Init();
+}
---
+++ tags/v0_1_8/LineTracer/resource.h
@@ -0,0 +1,88 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LineTracer.rc
+//
+#define IDD_PARAMDIALOG                 9
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_LineTracerTYPE              129
+#define IDR_TOOLBAR1                    133
+#define IDC_COMBOBOX                    134
+#define IDD_TOOLBOX                     135
+#define IDC_CLOSED_HAND                 138
+#define IDC_ZOOM_OUT                    145
+#define IDC_ZOOM_IN                     146
+#define IDC_ZOOM                        147
+#define IDI_MOVE                        148
+#define IDI_ICON1                       149
+#define IDI_ZOOM                        149
+#define IDI_ORIGINAL_LAYER              150
+#define IDI_VECTOR_LAYER                151
+#define IDI_ALL_LAYERS                  152
+#define IDI_VECTOR_LAYER1               154
+#define IDC_EDIT                        1000
+#define IDC_SPIN1                       1001
+#define IDC_BWTHRESHOLD                 1002
+#define IDC_MOVEBUTTON                  1003
+#define IDC_ZOOMBUTTON                  1004
+#define IDC_DETAILENHANCESLIDER        1005
+#define IDC_LINELENGTHSLIDER            1006
+#define IDC_TAILPRUNERSLIDER            1006
+#define IDC_SLIDER3                     1007
+#define IDC_CURVEDETAILSLIDER           1007
+#define IDC_HAND                        1007
+#define IDC_CUSTOM1                     1008
+#define IDC_DETAILENHANCESLIDER2       1008
+#define IDC_SLIDER1                     1009
+#define IDC_HOLEFILLERSLIDER            1013
+#define IDC_VIEW_ORIGINAL_BUTTON        1014
+#define IDC_EDIT1                       1016
+#define IDC_VIEW_RESULT_BUTTON          1017
+#define IDC_VIEW_ALL_BUTTON             1020
+#define IDC_EXPORT_TO_EPS_BUTTON        1021
+#define ID_OPERATIONS_BINARIZE          32771
+#define ID_OPERATION_BINARIZE           32772
+#define ID_FILE_OPENIMAGE               32773
+#define ID_PARAMETERS_BINARIZE          32774
+#define ID_PARAMETERS_GAUSSIANBLUR      32775
+#define ID_VIEW_GAUSSIAN                32776
+#define ID_VIEW_BINARIZER               32777
+#define ID_VIEW_SKELETONIZER            32778
+#define ID_FILE_EXPORTEPS               32779
+#define ID_VIEW_ORIGINAL                32780
+#define ID_PARAMETERS_LINESIZETHRESHOLD 32781
+#define ID_VIEW_ZOOM                    32782
+#define ID_PARAMETERS_NOISESURPRESSION  32785
+#define ID_VIEW_BEZIERMAKER             32786
+#define ID_VIEW_THINNER                 32787
+#define ID_PARAMETERS_TAILPRUNE         32788
+#define ID_PARAMETERS_CURVEDETAIL       32789
+#define ID_BUTTON32796                  32796
+#define ID_BUTTON32797                  32797
+#define ID_VIEW_                        32798
+#define ID_VIEW_TOOLBOX                 32799
+#define ID_EDIT_STEPFORWARD             32800
+#define ID_EDIT_STEPBACKWARD            32801
+#define ID_VIEW_TOOLBOX32802            32802
+#define ID_VIEW_ZOOMIN                  32803
+#define ID_VIEW_ZOOMOUT                 32804
+#define ID_VIEW_FITONSCREEN             32805
+#define ID_IMAGE_ROTATE                 32806
+#define ID_Menu                         32807
+#define ID_ROTATE_32808                 32808
+#define ID_ROTATE_91                    32809
+#define ID_ROTATE_92                    32810
+#define ID_FILE_OPENRECENT              32812
+#define ID_FITONSCREEN                  32819
+#define ID_FILE_CLOSEIMAGE              32821
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        158
+#define _APS_NEXT_COMMAND_VALUE         32824
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
