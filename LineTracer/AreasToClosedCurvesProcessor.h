5a2214ac00f17a77e303f0b29ec4f33d22b2e3ee
Johan <Johan@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2005-02-23 10:38:57 +0000

    implemented OK

--- LineTracer/AreasToClosedCurvesProcessor.h
+++ LineTracer/AreasToClosedCurvesProcessor.h
@@ -10,4 +10,7 @@ public:
 	static AreasToClosedCurvesProcessor* Instance();
 	~AreasToClosedCurvesProcessor(void);
 	CSketchImage* Process(CSketchImage* src);
+private:
+	void RemoveSmallAreasFromImage(CRawImage<bool> & a_image, const unsigned int a_minimumAreaThreshold);
+	void ScanArea(CRawImage<bool> &canvas, deque<int>* pixelsInArea, const CPoint &start, const unsigned int a_max_area) const;
 };
