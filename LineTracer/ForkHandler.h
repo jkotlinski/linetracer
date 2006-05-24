a7dabd79df0371b8c38fdbf61398b8085416d6d6
kotlinski <kotlinski@e5dc2c24-8a51-11dd-aa02-45bbecb07bf0>
2006-05-24 11:50:51 +0000

    removed unused SetEndPoint method

--- LineTracer/ForkHandler.h
+++ LineTracer/ForkHandler.h
@@ -23,10 +23,8 @@ public:
 private:
 	vector<CFPoint>* Find3Forks(const CLineImage *li) const;
 	vector<CFPoint>* ThreeForks;
-	//vector<CFPoint>* TForks;
 	vector<CFPoint>* YForks;
 	CLineImage* HandleTForks(const CLineImage* li);
-	void SetEndPoint(CPolyLine* pl, const CFPoint &p) const;
 	CLineImage* HandleYForks(const CLineImage* li);
 	void MarkYFork(CPolyLine* line, const CFPoint &p, int median) const;
 	void HandleYFork(CLineImage* img, CPolyLine* baseLine, CPolyLine* line1, CPolyLine* line2, const CFPoint &p);
